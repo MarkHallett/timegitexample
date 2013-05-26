62a94a3d3cc8fd580850c3ff82e648676402811e
Mark Hallett <git@markhallett.co.uk>
2013-05-27 00:14:26 +0100

    2 funcs for 2 tests

--- eg_code.py
+++ eg_code.py
@@ -2,10 +2,18 @@
 
 import time
 import datetime
+import random
 
-def eg_func():
+def eg_func1():
     print datetime.datetime.now()
     time.sleep(2)
     print datetime.datetime.now()
 
-eg_func()
+def eg_func2():
+    t = random.uniform(1,2)
+    print t
+    time.sleep(t)
+               
+               
+#if __main__ == '__name__':
+#    eg_func()
