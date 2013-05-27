e43c72e3073d66c3b58dd79e6fe07c6ac84d028b
Mark Hallett <git@markhallett.co.uk>
2013-05-27 09:57:20 +0100

    start of test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+# test.py
+
+import eg_code
+
+def test1():
+    eg_code.eg_func1()
+
+def test2():
+    eg_code.eg_func2()
+
+# --------------------------
+def run():
+    print 'test.run start'
+    test1()
+    test2()
+    print 'test.run end'
+    
+run()
